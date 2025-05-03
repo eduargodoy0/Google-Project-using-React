1e81e5c7cdb17a5fb79484245353f9cf00cfd6e8
eduargodoy0 <eduargodoy22@gmail.com>
2025-05-03 19:49:05 -0300

    Quarto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import './App.css'
-import Home from './components/home'
 
 function App() {
   return (
-    <Home></Home>
+    <div className="app-container">
+      <header className="header">
+        <a href="#">Gmail</a>
+        <a href="#">Imagens</a>
+        <span className="icon">☰</span>
+        <div className="profile-icon">E</div>
+      </header>
+
+      <div className="app">
+        <h1 className="logo">Google</h1>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Pesquise no Google ou digite uma URL"
+        />
+        <div className="buttons">
+          <button>Pesquisa Google</button>
+          <button>Estou com sorte</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const navbar = () => {
-  return (
-    <h1>navbar</h1>
-  )
-}
-
-export default navbar
